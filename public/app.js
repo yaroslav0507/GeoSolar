60b8e0f9c13ac51abcd29527582d1886b65f556a
Yaroslav Zaklinsky <yaroslav0507@gmail.com>
2015-03-26 07:24:29 +0200

    solar.stylweorks.com.ua

--- public/app.js
+++ public/app.js
@@ -4,13 +4,43 @@
 
 
 
-// Customer Object - contains information about
+// Client Object - contains information about
 // geolocation, solar station work period, GMT
 
-var Customer = {
+var Client = {
 	location : {
-		lat : 48.379433,
-		lng : 31.16558
+		lat : 37.09024,
+		lng : -95.712891,
+		avialable : "",
+		hack: function(){
+			// here will be a script of clients IP grab and getting an information about position by it.
+		},
+		get: function(){
+			var options = {
+				enableHightAccuracy: true,
+				timeout: 5000,
+			};
+
+			function success(pos){
+				var crd = pos.coords;
+
+				Client.location.lat = crd.latitude;
+				Client.location.lng = crd.longitude;
+				App.draw();
+
+				console.log('lat: ', crd.latitude, 'lng: ', crd.longitude);
+				Client.location.avialable = true;
+				
+			}
+
+			function error(err){
+				console.warn('ERROR('+ err.code + '): ' + err.message);
+				Client.location.avialable = false;
+			};
+
+			navigator.geolocation.getCurrentPosition(success, error, options);
+			
+		},
 	},
 	period: {
 		start: 80,
@@ -19,10 +49,10 @@ var Customer = {
 	// LST : 15,
 	// N 	: 80,
 	// GMT	: function(){
-	// 	return Math.floor(Customer.location.lng / 15);
+	// 	return Math.floor(Client.location.lng / 15);
 	// },
 	// DS  : function(){
-	// 	if (Customer.N < 90 && Customer.N < 300)
+	// 	if (Client.N < 90 && Client.N < 300)
 	// 		return 1 ;	
 	// 	else 
 	// 		return 0;
@@ -30,19 +60,17 @@ var Customer = {
 	// "hemisphere" : "",
 }
 
-// App object - contains calculations logics of the app
+// App object - contains math logic of the application
+// var switcher = true;
 
 var App = {
 
-	"Lat" :	Customer.location.lat,
-	"Lng" : Customer.location.lng,
-
 	// Declination of the Sun
-	//"decl" : Declination(Customer.N),
+	//"decl" : Declination(Client.N),
 
 	// True solar time
 	// TST  : function(){
-	// 	return Customer.LST + (this.Lat - 15 * Customer.GMT())/15 - Customer.DS();
+	// 	return Client.LST + (this.Lat - 15 * Client.GMT())/15 - Client.DS();
 	// },
 
 	// // Hour angle
@@ -58,10 +86,13 @@ var App = {
 	decl : function(){
 		//Array for further calculation of min and max declination
 		var decPerDay =  Array();
-		var startDay = 	Customer.period.start,
-			endDay = 	Customer.period.end;
+		var startDay = 	Client.period.start,
+			endDay = 	Client.period.end;
 		for (var i = 0; i <= endDay - startDay; i++){
 			decPerDay[i] = Declination(i + startDay); 
+			// if (switcher) {
+				decPerDay[i] < 0 ? decPerDay[i] = 0 : " ";
+			// };
 		};
 		return decPerDay;
 	},
@@ -73,7 +104,8 @@ var App = {
 		// (minDecl < 0) ? minDecl = 0 : " ";
 
 		var decl 	= Math.acos(0.5 * (cosd(minDecl) + cosd(maxDecl)));
-		bet = this.Lat - ToDeg(decl);
+
+		bet = Client.location.lat - ToDeg(decl);
 
 		return bet;
 	},
@@ -93,63 +125,62 @@ var App = {
 
 }
 
-// Toggle autocompleete feature when document is ready
-$(function(){
-	var input = document.getElementById('address');	
-	var autocomplete = new google.maps.places.Autocomplete(input);
-	App.draw();
-	geoApi.initialize();
-});	
+var geoApi = {
 
-var geoApi = {};
+	initialize: function(){
+
+		Client.location.get();
+		// Print initial information when client position is known
+		App.draw();
+
+		// Toggle autocompleete feature when document is ready
+		var input = document.getElementById('address');	
+		var autocomplete = new google.maps.places.Autocomplete(input);
+
+		// Creation of a coordinates for google map
+	    var geoLatlng = new google.maps.LatLng(lat, lng);
+	    var geoOptions = {
+	        zoom: Client.location.avialable ? 6 : 4,
+	        center: geoLatlng,
+	        mapTypeId: google.maps.MapTypeId.ROADMAP
+	    }
+	    // Initializing new google map canvas
+		map = new google.maps.Map(document.getElementById("map_canvas"), geoOptions);
+	   
+	},
+
+	geocode: function(){
+		var address = $('#address').val();
+		geocoder.geocode({
+			"address" : address
+		},
+		function(results, status){
+			$.ajax({
+		    	url: 	"http://maps.googleapis.com/maps/api/geocode/json?address="+address+"&sensor=false",
+		    	type: 	"POST",
+		    	success: function(res){
+		    		map.setCenter(res.results[0].geometry.location);
+		    		map.setZoom(14)
+					var marker = new google.maps.Marker({
+						map : map,
+						position : res.results[0].geometry.location
+					});
+					Client.location.lat = res.results[0].geometry.location.lat;
+					Client.location.lng = res.results[0].geometry.location.lng;
+
+					App.draw();
+		    	}
+		    });
+		});
+	}
+
+
+};
 
 var map;
-var lat = Customer.location.lat,
-	lng = Customer.location.lng;
+var lat = Client.location.lat,
+	lng = Client.location.lng;
 
 var geocoder = new google.maps.Geocoder();
-
-geoApi.initialize = function(){
-	// Creation of a coordinates for google map
-    var geoLatlng = new google.maps.LatLng(lat, lng);
-    var geoOptions = {
-        zoom: 6,
-        center: geoLatlng,
-        mapTypeId: google.maps.MapTypeId.ROADMAP
-    }
-    // Initializing new google map canvas
-	map = new google.maps.Map(document.getElementById("map_canvas"), geoOptions);
-    
-    // Placing a start marker to the initialized map
-	//  var marker = new google.maps.Marker({
-	// 	map : map,
-	// 	position : geoLatlng
-	// });
-}
-
-geoApi.geocode = function(){
-	var address = $('#address').val();
-	geocoder.geocode({
-		"address" : address
-	},
-	function(results, status){
-		$.ajax({
-	    	url: 	"http://maps.googleapis.com/maps/api/geocode/json?address="+address+"&sensor=false",
-	    	type: 	"POST",
-	    	success:function(res){
-	    		map.setCenter(res.results[0].geometry.location);
-	    		map.setZoom(14)
-				var marker = new google.maps.Marker({
-					map : map,
-					position : res.results[0].geometry.location
-				});
-				App.Lat = res.results[0].geometry.location.lat;
-				App.Lng = res.results[0].geometry.location.lng;
-
-				App.draw();
-	    	}
-	    });
-	});
-}
 
 google.maps.event.addDomListener(window, "load", geoApi.initialize);
--- public/interface.js
+++ public/interface.js
@@ -6,19 +6,19 @@
 App.draw = function(){
   $('#betta').html(DegsMinutes(App.betta()));
 
-  $('#latitide').val(Math.abs(App.Lat));
-  $('#longitude').val(Math.abs(App.Lng));
+  $('#latitide').val(Client.location.lat);
+  $('#longitude').val(Client.location.lng);
 
-  var from = Customer.period["start"],
-      to   = Customer.period["end"];
+  var from = Client.period["start"],
+      to   = Client.period["end"];
 
-  $('#period').html( "From " + dayToDate(Customer.period["start"]) + " - To " + dayToDate(Customer.period["end"]) );
+  $('#period').html( "From " + dayToDate(Client.period["start"]) + " - To " + dayToDate(Client.period["end"]) );
   $( "#slider-period" ).slider({
     values : [ from, to ]
   });
 
-  $('#from').val(dayToDate(Customer.period["start"]));
-  $('#to').val(dayToDate(Customer.period["end"]));
+  $('#from').val(dayToDate(Client.period["start"]));
+  $('#to').val(dayToDate(Client.period["end"]));
 
   $('.inclination .num').html(App.betta().toFixed() + "&deg;");
 
@@ -39,8 +39,8 @@ $(function() {
       max: 365,
       values: [ 80, 265 ],
       slide: function( event, ui ) {       
-        Customer.period.start = ui.values[ 0 ];
-        Customer.period.end = ui.values[ 1 ];
+        Client.period.start = ui.values[ 0 ];
+        Client.period.end = ui.values[ 1 ];
         App.draw();
       }
     });
@@ -66,7 +66,7 @@ $(function() {
             date[0] = a[0];
             date[1] = parseInt(a[1]);
         var dayNum = dateToDay(date);
-        Customer.period.start = dayNum;
+        Client.period.start = dayNum;
         App.draw();
       }
     });
@@ -84,15 +84,39 @@ $(function() {
             date[0] = a[0];
             date[1] = parseInt(a[1]);
         var dayNum = dateToDay(date);
-        Customer.period.end = dayNum;
+        Client.period.end = dayNum;
         App.draw();
       }
     });
 
-    $('.content').height($('.sidebar').height());
 
+    // Trigger search when 'Enter' button is hitted
+    $('input#address').keydown(function(e){
+      if (e.keyCode == 13){
+        $('#startSearch').trigger('click');
+      }
+    });
+
+    $('input#switch').on('click', function(){
+      $(this).is(':checked') == true ? switcher = true : switcher = false;
+      App.draw();
+    });
+
+    // Calculate initial scale of mobile device
+    if ( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|AppleWebKit/i.test(navigator.userAgent) ) {
+          var scale = window.innerWidth/640;
+          viewport = document.querySelector("meta[name=viewport]");
+          viewport.setAttribute('content', 'width=device-width, initial-scale='+scale+', user-scalable=0');
+    } 
+    
+    $('#centerPosition').on('click', function(){
+      Client.location.get();
+      geoApi.initialize();
+      App.draw();
+    });
+
+    // Redraw some page parts when window is resized
     $(window).resize(function(){
-      $('.content').height($('.sidebar').height());
       App.renderGraph();
     })
 });
